8ef2f4256f737b022345401b345bac181e2299be
Luan <calvin.nhieu@gmail.com>
2016-01-05 19:24:28 -0500

    fixed whitespace

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import Store from 'Store';
 
-export default class App extends React.Component{
+export default class App extends React.Component {
   render() {
     return (
       <Provider store={Store}>
