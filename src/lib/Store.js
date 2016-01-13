965c946f16a31c215b6a13c85ee9925d38ee0ebf
Luan <calvin.nhieu@gmail.com>
2016-01-12 23:15:31 -0500

    fix image load bug

--- src/lib/Store.js
+++ src/lib/Store.js
@@ -1,13 +1,11 @@
 import {createStore} from 'redux';
 
-let placeholder = 'https://scontent.cdninstagram.com/hphotos-xpf1/t51.2885-15/s640x640/sh0.08/e35/12501980_172924093067888_955910836_n.jpg';
-
 function reducer(state = [], action) {
   switch (action.type) {
   case 'LOADED':
     return action.data;
   default:
-    return [placeholder, placeholder];
+    return [];
   }
 }
 
